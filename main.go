bcf32d5fc9e6f0599c23ea2551bee988c95a12d5
Kendricko-adrio <kendrickoadrio134@gmail.com>
2021-12-08 17:03:41 +0700

    test

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ func main() {
 	// r.Use(middleware.CorsMiddleware)
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:3000"},
-		// AllowedOrigins:   []string{"https://managemesite.netlify.app"},
+		// AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"https://managemesite.netlify.app"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "PUT", "POST"},
 	})
